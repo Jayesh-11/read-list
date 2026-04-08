f0bccfe6a49421fd8c7b00a5f872ef38831cd54d
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-04-09 01:06:54 +0530

    testing gh actions

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,8 @@ const config = {
   kit: {
     adapter: adapter({
       fallback: "404.html",
+      pages: "dist",
+      assets: "dist",
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
