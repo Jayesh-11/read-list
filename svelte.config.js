f7f77fa391622d8cc0e5a99f22958a05c5b4ff67
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-04-08 14:23:40 +0530

    testing gh actions

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.argv.includes("dev") ? "" : (process.env.BASE_PATH ?? ""),
     },
   },
 };
