e3877461f8c191a4c156ad8c8a3cf83f78253e7d
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-04-11 23:30:40 +0530

    added footer section

--- src/App.svelte
+++ src/App.svelte
@@ -1,131 +1,17 @@
 <script lang="ts">
-  import list from "./list.json";
-  import MiniSearch from "minisearch";
-  const miniSearch = new MiniSearch({
-    fields: ["title", "tags", "notes", "createdAt", "url", "author"],
-    storeFields: ["title", "tags", "notes", "createdAt", "url", "author"],
-  });
-  miniSearch.addAll(list);
-
-  let query: string = $state("");
-  let tagsFilter: string[] = $state([]);
-
-  const results = $derived.by(() => {
-    const tagsFilterSet = new Set(tagsFilter);
-    if (!query) {
-      if (tagsFilter.length === 0) return list;
-      return list.filter((item) => {
-        return item.tags.some((tag: string) => tagsFilterSet.has(tag));
-      });
-    }
-    const queryResults = miniSearch.search(query, {
-      filter: (result) => {
-        if (tagsFilter.length === 0) return result;
-        return result.tags.some((tag: string) => tagsFilterSet.has(tag));
-      },
-    });
-    return queryResults;
-  });
-
-  const onInput = (q: string) => {};
-  const onTagClickHandler = (tag: string) => {
-    if (tagsFilter.includes(tag)) {
-      tagsFilter = tagsFilter.filter((t) => t !== tag);
-      return;
-    }
-    tagsFilter.push(tag);
-  };
+  import Footer from "./Footer.svelte";
+  import List from "./List.svelte";
 </script>
 
-<div class="container">
-  <input
-    class="query-input"
-    bind:value={query}
-    oninput={(e) => onInput(e.currentTarget.value)}
-    placeholder="Search anything..."
-  />
-  <div class="tag-filter-view">
-    {#each tagsFilter as filterTag (filterTag)}
-      <button class="tag" data-tagname={filterTag}>{filterTag}</button>
-    {/each}
-  </div>
-
-  {#each results as result}
-    <div class="card">
-      <a href={result.url} target="_blank">{result.title}</a>
-      <span>{result.author}</span>
-      <div class="tag-container">
-        {#each result.tags as tag}
-          <button
-            class="tag"
-            data-tagname={tag}
-            onclick={() => onTagClickHandler(tag)}>{tag}</button
-          >
-        {/each}
-      </div>
-    </div>
-  {/each}
+<div class="primary">
+  <List />
+  <Footer />
 </div>
 
 <style>
-  .card {
+  .primary {
     display: flex;
     flex-direction: column;
-    gap: 8px;
-    padding: 8px;
-    max-width: 100%;
-    border: 1.5px solid #000;
-    box-shadow: 2px 2px 0 #000;
-    outline: none;
-    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  }
-
-  .card:hover {
-    box-shadow: 4px 4px 0 rgba(0, 0, 0);
-  }
-
-  .container {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    gap: 16px;
-  }
-
-  .query-input {
-    font-size: 16px;
-    padding: 12px;
-    max-width: 100%;
-    border: 1.5px solid #000;
-    box-shadow: 2px 2px 0 #000;
-    outline: none;
-    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  }
-
-  .query-input:hover,
-  .query-input:focus {
-    box-shadow: 4px 4px 0 rgba(0, 0, 0);
-  }
-
-  .tag-container {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-  }
-
-  .tag {
-    border: none;
-    cursor: pointer;
-    font-size: 14px;
-    padding: 4px 8px;
-    font-family: monospace;
-  }
-
-  .tag[data-tagname="React"] {
-    background-color: rgb(97, 219, 251);
-  }
-
-  .tag-filter-view {
-    display: flex;
     gap: 8px;
   }
 </style>
---
+++ src/Footer.svelte
@@ -0,0 +1,73 @@
+<div>
+  <button class="footer-button" popovertarget="about" commandfor="about"
+    >About</button
+  >
+  <button class="footer-button" command="show-modal" commandfor="add-new"
+    >Add new</button
+  >
+  <a href="https://github.com/Jayesh-11/read-list">Github</a>
+</div>
+
+<a href="https://j0dev.com/">By Jayesh Vyavahare</a>
+
+<dialog id="add-new">
+  <p>WIP form</p>
+  <button commandfor="my-dialog" command="close">Close</button>
+</dialog>
+
+<div class="about-popover" id="about" popover="hint">
+  <p>
+    I used to manage all these links in a markdown file, while easy to manage,
+    there were some problems, also there was no scaling issue, as realistically
+    there wouldn't be &gt;10000+ links.
+  </p>
+  <ol>
+    <li>How do you search what you need from over 300+ links</li>
+    <li>How do you share these links easily</li>
+    <li>I don't want to go to github to share link</li>
+    <li>I wanted notes based search and tag based search</li>
+    <li>Share the whole list with someone</li>
+    <li>Easily add to the list</li>
+    <li>Aesthetics</li>
+  </ol>
+
+  <p>
+    Also I wanted to test drive Svelte, so learnt via building, and this was the
+    outcome, <strong>intentionally kept it simple</strong>, fork, star, build
+    your own read list.
+  </p>
+  <button
+    class="close-button"
+    popovertarget="about"
+    popovertargetaction="hide"
+    command="hide">Close</button
+  >
+</div>
+
+<style>
+  .footer-button {
+    padding: 4px 8px;
+    border: 1.5px solid #000;
+    box-shadow: 2px 2px 0 #000;
+    outline: none;
+    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    cursor: pointer;
+  }
+  .about-popover {
+    gap: 8px;
+    padding: 8px;
+    max-width: 60vh;
+    border: 2px solid #000;
+    box-shadow: 2px 2px 0 #000;
+    outline: none;
+    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+  .close-button {
+    border: 1.5px solid #000;
+    box-shadow: 2px 2px 0 #000;
+    outline: none;
+    cursor: pointer;
+    font-size: 16px;
+    padding: 4px 8px;
+  }
+</style>
